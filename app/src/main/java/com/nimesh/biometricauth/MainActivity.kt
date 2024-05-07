8e7a34d3b44a8d360098e1c6665a0bf02a7286b0
Nimesh Shah <nimesh.shah@teachmint.com>
2024-05-07 12:15:53 +0530

    Initial commit

---
+++ app/src/main/java/com/nimesh/biometricauth/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.nimesh.biometricauth
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.nimesh.biometricauth.BiometricPromptManager.BiometricResult
+import com.nimesh.biometricauth.ui.theme.BiometricAuthTheme
+
+class MainActivity : AppCompatActivity() {
+
+    private val promptManager by lazy {
+        BiometricPromptManager(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            BiometricAuthTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val biometricResult by promptManager.promptResults.collectAsState(
+                        initial = null
+                    )
+                    val enrollLauncher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.StartActivityForResult(),
+                        onResult = {
+                            println("Activity result: $it")
+                        }
+                    )
+                    LaunchedEffect(biometricResult) {
+                        if (biometricResult is BiometricPromptManager.BiometricResult.AuthenticationNotSet) {
+                            if (Build.VERSION.SDK_INT >= 30) {
+                                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                                    putExtra(
+                                        Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL
+                                    )
+                                }
+                                enrollLauncher.launch(enrollIntent)
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick = {
+                            promptManager.showBiometricPrompt(
+                                title = "Sample prompt",
+                                description = "Sample prompt description"
+                            )
+                        }) {
+                            Text(text = "Authenticate")
+                        }
+                        biometricResult?.let { result ->
+                            Text(
+                                text = when (result) {
+                                    is BiometricResult.AuthenticationError -> {
+                                        result.error
+                                    }
+
+                                    BiometricResult.AuthenticationFailed -> {
+                                        "Authentication failed"
+                                    }
+
+                                    BiometricResult.AuthenticationNotSet -> {
+                                        "Authentication not set"
+                                    }
+
+                                    BiometricResult.AuthenticationSuccess -> {
+                                        "Authentication success"
+                                    }
+
+                                    BiometricResult.FeatureUnavailable -> {
+                                        "Feature unavailable"
+                                    }
+
+                                    BiometricResult.HardwareUnavailable -> {
+                                        "Hardware unavailable"
+                                    }
+                                }
+                            )
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
